abab18c7993bfb0413ffbb82c4cf524ba37c0fcd
gavin <clarkhillm@gmail.com>
2015-04-07 11:03:25 +0800

    补充主机以及交换机的关系。

--- src/main/resources/js/execute/relation/switchLinkInfo.js
+++ src/main/resources/js/execute/relation/switchLinkInfo.js
@@ -21,9 +21,37 @@ induction.relation.switchLinkInfo = [
                         return _.chain(switches).filter(function (ss) {
                             return ss.WWN === psp.WWN;
                         }).map(function (xx) {
-                            return {name: xx.name, switchIp: xx.ip, sPort: psp.name};
+                            var selfPort = _.chain(xx.ports)
+                                .filter(function (x) {
+                                    return x.WWN === xx.WWN;
+                                }).map(function (sp) {
+                                    return {
+                                        WWN: sp.WWN,
+                                        slotID: sp.SLOT_ID,
+                                        portID: sp.PORT_ID,
+                                        name: sp.NAME,
+                                        elementID: sp.ELEMENT_ID
+                                    }
+                                }).first().value();
+                            if (!selfPort) {
+                                selfPort = {
+                                    WWN: '*',
+                                    slotID: '*',
+                                    portID: '*',
+                                    name: '*',
+                                    elementID: '*'
+                                }
+                            }
+                            return {
+                                name: xx.name,
+                                switchIp: xx.ip,
+                                sPort: psp.name,
+                                portName: selfPort.name,
+                                WWN: selfPort.WWN
+                            };
                         }).value();
                     }));
+
                     rs.linkstor = _.flatten(_.map(prime_switch.ports, function (psp) {
                         return _.chain(storages).filter(function (s) {
                             return psp.WWN === s.PERMANENT_ADDRESS;
@@ -37,11 +65,18 @@ induction.relation.switchLinkInfo = [
                             };
                         }).value();
                     }));
+
                     rs.linkhost = _.flatten(_.map(prime_switch.ports, function (psp) {
                         return _.chain(hosts).filter(function (s) {
                             return psp.WWN === s.WWN;
                         }).map(function (ss) {
-                            return {name: ss.NAME, hostIp: ss.IP, sPort: psp.name};
+                            return {
+                                name: ss.NAME,
+                                hostIp: ss.IP,
+                                sPort: psp.name,
+                                portName: ss.PORTNAME,
+                                WWN: ss.WWN
+                            };
                         }).value();
                     }));
                     return rs;
@@ -70,10 +105,34 @@ induction.relation.switchLinkInfo = [
                                     mainNodeType: 'switch',
                                     mainsPort: stor.sPort,
                                     localWwn: s.WWN,
-                                    otherNodeName: stor.ip + stor.stroIp,
+                                    otherNodeName: stor.storIp + stor.name,
                                     otherNodeType: 'storage',
                                     otherPortName: stor.port,
                                     otherWwn: stor.WWN
+                                }
+                            }));
+                            rs = rs.concat(_.map(s.linkswitch, function (ss) {
+                                return {
+                                    mainNodeName: s.ip + s.name,
+                                    mainNodeType: 'switch',
+                                    mainsPort: ss.sPort,
+                                    localWwn: s.WWN,
+                                    otherNodeName: ss.switchIp + ss.name,
+                                    otherNodeType: 'switch',
+                                    otherPortName: ss.portName,
+                                    otherWwn: ss.WWN
+                                }
+                            }));
+                            rs = rs.concat(_.map(s.linkhost, function (h) {
+                                return {
+                                    mainNodeName: s.ip + s.name,
+                                    mainNodeType: 'switch',
+                                    mainsPort: h.sPort,
+                                    localWwn: s.WWN,
+                                    otherNodeName: h.hostIp + h.name,
+                                    otherNodeType: 'host',
+                                    otherPortName: h.portName,
+                                    otherWwn: h.WWN
                                 }
                             }));
                             return JSON.stringify(rs);
